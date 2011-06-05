61bd67ecf2cf59c9a4ec314b0c636172ff135509
Chris Constantine <cconstan@gmail.com>
2011-06-05 15:57:07 -0700

    tweaks

--- mysite/handle/static/graph.js
+++ mysite/handle/static/graph.js
@@ -1,15 +1,18 @@
-function AjaxGraph(doc, url, totalPoints, updateInterval) {
+function AjaxGraph(doc, url) {
     // setup plot
     var options = {
-	series: { shadowSize: 0 },
+	series: { shadowSize: 0,
+		  lines: { show: true },
+		  points: { show: true }
+        },
 	xaxis: {
 	    mode: 'time',
 	    twelveHourClock: true
 	},
 	ticks: 10,
 	updater: {
-	    updateInterval: updateInterval,
-	    totalPoints: totalPoints,
+	    updateInterval: 1000,
+	    totalPoints: 50,
 	    ajax: {
 		url: 'api',
 		timeout: 2000,
